cfa00a1f32c6d51a66f0f894fbd6e5148c1ec5fa
Lachezar Ivanov <128148925+LachezarIvanovv@users.noreply.github.com>
2023-11-22 09:22:29 +0200

    Complete and working version

---
+++ src/components/ToDoTable.js
@@ -0,0 +1,28 @@
+import TodoRowItem from "./TodoRowItem"
+
+function ToDoTable(props){
+    return(
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Task</th>
+                    <th scope='col'>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.todos.map(todo => (
+                    <TodoRowItem
+                        key={todo.rowNumber}
+                        rowNumber={todo.rowNumber}
+                        rowDescription={todo.rowDescription}
+                        rowAssigned={todo.rowAssigned}
+                        deleteTodo={props.deleteTodo}
+                    />
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default ToDoTable
